6a414a6c0ebf431eeebe1e419fe9a460afa0b69d
Jonathan Tseng <jtt24@duke.edu>
2014-11-20 02:19:38 -0500

    MultiLanguageUtility
    Utility for allowing multi-language support in JavaFX GUIs. Uses the JavaFX ObjectProperty class and ResourceBundles to allow for dynamically changing the language in the GUI.

---
+++ src/LanguageException/LanguageException.java
@@ -0,0 +1,14 @@
+package LanguageException;
+
+public abstract class LanguageException extends Exception {
+
+    /**
+     * Auto-generated
+     */
+    private static final long serialVersionUID = 1L;
+
+    protected LanguageException (String message) {
+        super(message);
+    }
+
+}
